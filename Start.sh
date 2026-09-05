e2285c928028ab7b41fd9adcdbd439d83ef91595
Athrun Kyle Novicio Sison <111546204+Nurtha@users.noreply.github.com>
2026-09-05 23:59:37 +0800

    Update Start.sh to set GOOGLE_CLIENT_ID and change command

--- Start.sh
+++ Start.sh
@@ -1,1 +1,2 @@
-node script.js & ~/cloudflared tunnel run --url http://localhost:3000 nurtha-site
+export GOOGLE_CLIENT_ID="158420700965-5ft92v6dtf2b18j0j9taq8g33qqit3r3.apps.googleusercontent.com"
+node run start & ~/cloudflared tunnel run --url http://localhost:3000 nurtha-site   
